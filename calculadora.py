7d99a3b12df4dbcacf2c180ff0031f2b315ff1f9
iparraguirrejuan <juan.ciparraguirre@gmail.com>
2023-03-04 07:38:35 -0300

    Menu de inicio para aplicacion

---
+++ calculadora.py
@@ -0,0 +1,42 @@
+import sys
+import mysql.connector
+from funciones import App
+
+
+
+
+while True:
+    print("""
+Bienvenido Dr/a a la calculadora antropometrica
+
+1 - Ingrese un nuevo paciente
+2 - Hacer una antropometria
+3 - Ver un paciente
+4 - Salir
+        """)
+        
+    opcion = int (input("Seleccione el numero de la operacion a realizar "))
+
+    
+    if opcion == 1:
+        print("<<<< Insertar nuevo paciente >>>>")  
+        
+
+    elif opcion == 2:
+        print("")
+        print(">>>> Hacer una antropometria <<<<")
+        # Poner todos los datos que vamos a utilizar para una antropometira, todas las variables que vamos a utilizar, luego hacer todas las funciones 
+
+    elif opcion == 3:
+        print("")
+        print("<<<< Lista de pacientes >>>>")
+        print("")
+
+    elif opcion == 4:
+        print("Muchas gracias por usar nuestro programa")
+        sys.exit()
+
+    else:
+        print("La opcion es incorrecta.... Por favor ingrese una opcion correcta")
+
+        
